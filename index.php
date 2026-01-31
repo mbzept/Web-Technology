2ce080c063cd02c2cc6b559b7729281d657f39db
Anshuman Singh <asuryavanshi7398@gmail.com>
2026-02-01 00:13:59 +0530

    Add registration form in index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<form action="register.php" method="POST">
+    <h2>Register</h2>
+    <input type="text" name="username" required>
+    <input type="email" name="email" required>
+    <input type="password" name="password" required>
+    <button type="submit">Register</button>
+    <p><a href="login.php">Login</a></p>
+</form>
+
+</body>
+</html>
